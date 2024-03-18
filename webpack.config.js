8f30d5a2edf371c942368e50adc185e3422f39ce
anna <anna.senchukova@goods.ru>
2024-03-18 12:42:31 +0300

    master: Добавлена структура, зависимости и .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/js/index.js', // Точка входа для сборки проекта
+
+    output: {
+        filename: 'bundle.js', // Имя выходного файла сборки
+        path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i, // Регулярное выражение для обработки файлов с расширением .css
+                use: [MiniCssExtractPlugin.loader, 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/js/index.html',
+        }),
+
+        new MiniCssExtractPlugin (),
+    ],
+
+    devServer: {
+        port: 3000,
+        static: {
+            directory: path.join(__dirname, 'dist'), // Каталог для статики
+        },
+        open: true, // Автоматически открывать браузер
+    },
+
+    mode: 'development', // Режим сборки
+};
