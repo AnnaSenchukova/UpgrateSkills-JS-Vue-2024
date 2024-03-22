d8222d576246b19a9787fe90265e2fcef51f2e3c
anna <anna.senchukova@goods.ru>
2024-03-22 20:42:01 +0300

    master-webpack: добавила плагин cleanWebpack для очистки папки dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development', // Режим сборки
     entry: './src/js/index.js', // Точка входа для сборки проекта
 
     output: {
-        filename: 'bundle.js', // Имя выходного файла сборки
+        filename: '[name].js', // Имя выходного файла сборки
         path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
     },
 
@@ -21,10 +23,12 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/js/index.html',
+            template: './src/js/index.html' ,
         }),
 
         new MiniCssExtractPlugin (),
+        new CleanWebpackPlugin (),
+
     ],
 
     devServer: {
@@ -35,5 +39,5 @@ module.exports = {
         open: true, // Автоматически открывать браузер
     },
 
-    mode: 'development', // Режим сборки
+
 };
